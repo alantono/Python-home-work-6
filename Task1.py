c3738ee706608a66883cec54df2dd88e698c50ce
Alexey <antonov1972@yandex.ru>
2022-12-26 12:13:25 +0300

    Task1 update

--- Task1.py
+++ Task1.py
@@ -15,8 +15,6 @@
 # Решение (задачи приведенной выше) с помощью использования **лямбд, filter, map, zip, enumerate, list comprehension
 
 n = int(input('Введите n: '))
-list1 = []
-list2 = []
 list1 = [i for i in range(1, n + 1)]
 list2 = [round(((1 + (1 / i))**i), 2) for i in range(1, n + 1)]
 result = list(zip(list1, list2))
